d5ba1c0b226fd0ba833adf95c54841cd905bd80e
Sachin Fernandes <sachin@cloudflare.com>
2019-01-15 04:32:55 -0800

    Added webpack, babel and jest

---
+++ __tests__/index.js
@@ -0,0 +1,3 @@
+test("placeholder test", () => {
+  expect("").toBe("");
+});
--- webpack.config.js
+++ webpack.config.js
@@ -5,5 +5,8 @@ module.exports = {
   output: {
     filename: "main.js",
     path: path.resolve(__dirname, "dist")
+  },
+  module: {
+    rules: [{ test: /\.js$/, exclude: /node_modules/, loader: "babel-loader" }]
   }
 };
